2ce66d197a2d7bdb8faef736d8bbd61c97c0463f
Marius Gripsgard <me@mariogrip.com>
2016-03-22 14:44:39 +0100

    init commit

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE := file_contexts
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
